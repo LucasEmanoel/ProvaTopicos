d518ee79e577dca984a75c202e76392db53c598f
LUCAS EMANOEL SILVA BARROS <20171d13gr0225@LABS.GARANHUNS.IFPE>
2018-12-07 20:44:49 -0300

    closes #1

---
+++ src/prova/Aluno.java
@@ -0,0 +1,25 @@
+package prova;
+
+public class Aluno {
+
+	private String nome;
+	private String matricula;
+	
+	public Aluno() {
+		
+	}
+	public Aluno(String nome, String mat) {
+		this.nome = nome;
+		this.matricula = mat;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}	
+	
+	
+
+}
---
+++ src/prova/Principal.java
@@ -0,0 +1,49 @@
+package prova;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Aluno> alunos = new ArrayList();
+		int op = 0;
+		String nome = "";
+		String matricula = "";
+		
+		Aluno aluno = null;
+		
+		do {
+			System.out.println("1 - cadastrar\n"+
+								"2 - listar\n" + 
+								"3 - deletar\n" +
+								"0 - sair");
+			op = sc.nextInt();
+			switch(op) {
+			case 1:
+				System.out.println("Digite o nome do aluno");
+				nome = sc.next();
+				System.out.println("Digite a matricula do aluno");
+				matricula = sc.next();
+				
+				aluno = new Aluno(nome, matricula);
+				cadastroCliente(alunos, aluno);
+
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				break;
+			default:
+			}
+		}while(op != 0);
+	}
+	public static void cadastroCliente(List<Aluno> list, Aluno aluno) {
+		if(!(list.contains(aluno.getMatricula()))) 
+			list.add(aluno);
+			
+	}
+}
